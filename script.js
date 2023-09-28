97119cd5c8e39b9dedd36df330ed3c4008383b32
Žan Horvat <zhorvat0716@gmail.com>
2023-09-28 22:28:20 +0200

    ✨ Refresh

--- script.js
+++
@@ -1,7 +0,0 @@
-function copyToClipboard() {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val("zan.horvat@outlook.com").select();
-    document.execCommand("copy");
-    $temp.remove();
-}
